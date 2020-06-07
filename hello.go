a262e1504e4b7dfd2cce69498b1e7250df3b9785
kacperf531 <kacperf531@github.com>
2020-06-07 16:29:21 +0200

    Hello now supports different languages

--- hello.go
+++ hello.go
@@ -2,15 +2,37 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const french = "French"
+const finnish = "Finnish"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+const finnishHelloPrefix = "Hei, "
 
-func Hello(name string) string {
+
+func greetingPrefix(language string) (prefix string) {
+    switch language {
+    case french:
+        prefix = frenchHelloPrefix
+    case spanish:
+		prefix = spanishHelloPrefix
+	case finnish:
+		prefix = finnishHelloPrefix
+    default:
+        prefix = englishHelloPrefix
+    }
+    return
+}
+
+func Hello(name string, language string) string {
     if name == "" {
         name = "World"
     }
-    return englishHelloPrefix + name
+
+    return greetingPrefix(language) + name
 }
 
 func main() {
-    fmt.Println(Hello("world"))
+    fmt.Println(Hello("world", ""))
 }
