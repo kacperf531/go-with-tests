552b7eee70880b8dd981a9be34bdb63e7dd6eef7
kacperf531 <k.frackowski@livechatinc.com>
2021-12-03 17:11:00 +0100

    Introduce Update interface for Dictionary

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -3,8 +3,9 @@ package maps
 type Dictionary map[string]string
 
 var (
-	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
-	ErrWordExists = DictionaryErr("cannot add word because it already exists")
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it doesn't exist")
 )
 
 type DictionaryErr string
@@ -30,6 +31,21 @@ func (d Dictionary) Add(word, definition string) error {
 		d[word] = definition
 	case nil:
 		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case nil:
+		d[word] = definition
+	case ErrNotFound:
+		return ErrWordDoesNotExist
 	default:
 		return err
 	}
