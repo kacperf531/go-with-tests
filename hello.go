2178afd8ba4ce8643ec6fcdb03c8d82c27a309e5
kacperf531 <k.frackowski@livechatinc.com>
2022-01-04 12:31:26 +0100

    Use example HTTP handler in the main package

--- hello.go
+++ hello.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
 
 const spanish = "Spanish"
 const french = "French"
@@ -10,29 +15,36 @@ const spanishHelloPrefix = "Hola, "
 const frenchHelloPrefix = "Bonjour, "
 const finnishHelloPrefix = "Hei, "
 
-
 func greetingPrefix(language string) (prefix string) {
-    switch language {
-    case french:
-        prefix = frenchHelloPrefix
-    case spanish:
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
 		prefix = spanishHelloPrefix
 	case finnish:
 		prefix = finnishHelloPrefix
-    default:
-        prefix = englishHelloPrefix
-    }
-    return
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func Hello(name string, language string) string {
-    if name == "" {
-        name = "World"
-    }
+	if name == "" {
+		name = "World"
+	}
 
-    return greetingPrefix(language) + name
+	return greetingPrefix(language) + name
+}
+
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
 }
 
 func main() {
-    fmt.Println(Hello("world", ""))
+	log.Fatal(http.ListenAndServe(":5000", http.HandlerFunc(MyGreeterHandler)))
 }
