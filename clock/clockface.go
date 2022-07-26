732b2399581b8a215a46b0a9a38c34ceb000ceb4
kacperf531 <k.frackowski@livechatinc.com>
2022-07-26 16:09:47 +0200

    Add acceptance test for minute hand in clock

--- clock/clockface.go
+++ clock/clockface.go
@@ -68,3 +68,8 @@ func secondHandPoint(t time.Time) Point {
 func secondsInRadians(t time.Time) float64 {
 	return (math.Pi / (30 / (float64(t.Second()))))
 }
+
+func minutesInRadians(t time.Time) float64 {
+	return (secondsInRadians(t) / 60) +
+		(math.Pi / (30 / float64(t.Minute())))
+}
