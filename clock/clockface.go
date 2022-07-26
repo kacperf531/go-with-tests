e63ac0a7fafa3f4e70a3f1091071c5b0a41293e1
kacperf531 <k.frackowski@livechatinc.com>
2022-07-26 16:31:28 +0200

    Add hoursInRadians implementation

--- clock/clockface.go
+++ clock/clockface.go
@@ -90,3 +90,7 @@ func minutesInRadians(t time.Time) float64 {
 	return (secondsInRadians(t) / 60) +
 		(math.Pi / (30 / float64(t.Minute())))
 }
+
+func hoursInRadians(t time.Time) float64 {
+	return (math.Pi / (6 / (float64(t.Hour() % 12))))
+}
