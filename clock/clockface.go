bbf0cc4391b5fd97a7f9f4e7ea36d4f1c8a0e97c
kacperf531 <k.frackowski@livechatinc.com>
2022-07-26 13:05:19 +0200

    Add basic implementation for the clock hand showing seconds

--- clock/clockface.go
+++ clock/clockface.go
@@ -1,6 +1,9 @@
 package clockface
 
-import "time"
+import (
+	"math"
+	"time"
+)
 
 // A Point represents a two dimensional Cartesian coordinate
 type Point struct {
@@ -8,8 +11,28 @@ type Point struct {
 	Y float64
 }
 
+const secondHandLength = 90
+const clockCentreX = 150
+const clockCentreY = 150
+
 // SecondHand is the unit vector of the second hand of an analogue clock at time `t`
 // represented as a Point.
 func SecondHand(t time.Time) Point {
-	return Point{150, 60}
+	p := secondHandPoint(t)
+	p = Point{p.X * secondHandLength, p.Y * secondHandLength}
+	p = Point{p.X, -p.Y}
+	p = Point{p.X + clockCentreX, p.Y + clockCentreY} //translate
+	return p
+}
+
+func secondHandPoint(t time.Time) Point {
+	angle := secondsInRadians(t)
+	x := math.Sin(angle)
+	y := math.Cos(angle)
+
+	return Point{x, y}
+}
+
+func secondsInRadians(t time.Time) float64 {
+	return (math.Pi / (30 / (float64(t.Second()))))
 }
