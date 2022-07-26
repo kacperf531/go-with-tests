2bfd2396fca2ff0f2db12de7a85b243feca03d0f
kacperf531 <k.frackowski@livechatinc.com>
2022-07-26 16:38:47 +0200

    Add hourHand implementation

--- clock/clockface_test.go
+++ clock/clockface_test.go
@@ -14,45 +14,6 @@ func testName(t time.Time) string {
 	return t.Format("15:04:05")
 }
 
-func TestSecondHandAtMidnight(t *testing.T) {
-	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
-
-	want := Point{X: 150, Y: 150 - 90}
-	got := SecondHand(tm)
-
-	if got != want {
-		t.Errorf("Got %v, wanted %v", got, want)
-	}
-}
-
-func TestSecondHandAt30Seconds(t *testing.T) {
-	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
-
-	want := Point{X: 150, Y: 150 + 90}
-	got := SecondHand(tm)
-
-	if got != want {
-		t.Errorf("Got %v, wanted %v", got, want)
-	}
-}
-
-func TestSecondHandVector(t *testing.T) {
-	cases := []struct {
-		time  time.Time
-		point Point
-	}{
-		{simpleTime(0, 0, 30), Point{0, -1}},
-	}
-
-	for _, c := range cases {
-		t.Run(testName(c.time), func(t *testing.T) {
-			got := secondHandPoint(c.time)
-			if got != c.point {
-				t.Fatalf("Wanted %v Point, but got %v", c.point, got)
-			}
-		})
-	}
-}
 func TestSecondHandPoint(t *testing.T) {
 	cases := []struct {
 		time  time.Time
