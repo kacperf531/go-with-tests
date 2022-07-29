1b984da9a07c3144ebffa58bc7e0fbc60c43a6fc
kacperf531 <k.frackowski@livechatinc.com>
2022-07-29 17:14:18 +0200

    Handle reading post descriptions

--- blogposts/blogposts_test.go
+++ blogposts/blogposts_test.go
@@ -16,9 +16,16 @@ func assertPost(t *testing.T, got blogposts.Post, want blogposts.Post) {
 }
 
 func TestNewBlogPosts(t *testing.T) {
+	const (
+		firstBody = `Title: Post 1
+Description: Description 1`
+		secondBody = `Title: Post 2
+Description: Description 2`
+	)
+
 	fs := fstest.MapFS{
-		"hello world.md":  {Data: []byte("Title: Post 1")},
-		"hello-world2.md": {Data: []byte("Title: Post 2")},
+		"hello world.md":  {Data: []byte(firstBody)},
+		"hello-world2.md": {Data: []byte(secondBody)},
 	}
 
 	posts, err := blogposts.NewPostsFromFS(fs)
@@ -31,5 +38,9 @@ func TestNewBlogPosts(t *testing.T) {
 		t.Errorf("got %d posts, wanted %d posts", len(posts), len(fs))
 	}
 
-	assertPost(t, posts[0], blogposts.Post{Title: "Post 1"})
+	assertPost(t, posts[0], blogposts.Post{
+		Title:       "Post 1",
+		Description: "Description 1",
+	})
+
 }
