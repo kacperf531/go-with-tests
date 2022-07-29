fe5c26dfdc5322099842f6fa22f515a6b027c3fb
kacperf531 <k.frackowski@livechatinc.com>
2022-07-29 17:15:46 +0200

    Refactoring: remove repetition & magic numbers

--- blogposts/blogposts.go
+++ blogposts/blogposts.go
@@ -36,14 +36,21 @@ func getPost(fileSystem fs.FS, fileName string) (Post, error) {
 	return newPost(postFile)
 }
 
+const (
+	titleSeparator       = "Title: "
+	descriptionSeparator = "Description: "
+)
+
 func newPost(postFile io.Reader) (Post, error) {
 	scanner := bufio.NewScanner(postFile)
 
-	scanner.Scan()
-	titleLine := scanner.Text()
+	readLine := func() string {
+		scanner.Scan()
+		return scanner.Text()
+	}
 
-	scanner.Scan()
-	descriptionLine := scanner.Text()
+	title := readLine()[len(titleSeparator):]
+	description := readLine()[len(descriptionSeparator):]
 
-	return Post{Title: titleLine[7:], Description: descriptionLine[13:]}, nil
+	return Post{Title: title, Description: description}, nil
 }
