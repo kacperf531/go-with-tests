291f38d5cc1bc612e6fce02634fc53d34ab591bf
kacperf531 <k.frackowski@livechatinc.com>
2022-08-01 14:13:16 +0200

    Use template to render the blogpost

--- blogrenderer/blogrenderer.go
+++ blogrenderer/blogrenderer.go
@@ -1,7 +1,7 @@
 package blogrenderer
 
 import (
-	"fmt"
+	"html/template"
 	"io"
 )
 
@@ -10,7 +10,19 @@ type Post struct {
 	Tags                     []string
 }
 
+const (
+	postTemplate = `<h1>{{.Title}}</h1><p>{{.Description}}</p>Tags: <ul>{{range .Tags}}<li>{{.}}</li>{{end}}</ul>`
+)
+
 func Render(w io.Writer, p Post) error {
-	_, err := fmt.Fprintf(w, "<h1>%s</h1>", p.Title)
-	return err
+	templ, err := template.New("blog").Parse(postTemplate)
+	if err != nil {
+		return err
+	}
+
+	if err := templ.Execute(w, p); err != nil {
+		return err
+	}
+
+	return nil
 }
