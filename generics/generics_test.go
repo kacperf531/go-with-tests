ceea9bcf9dd02524516fe651c1cd802cc90f7359
kacperf531 <k.frackowski@livechatinc.com>
2022-08-02 14:42:31 +0200

    Use generic data type for Stack

--- generics/generics_test.go
+++ generics/generics_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestStack(t *testing.T) {
 	t.Run("integer stack", func(t *testing.T) {
-		myStackOfInts := new(StackOfInts)
+		myStackOfInts := new(Stack[int])
 
 		// check stack is empty
 		AssertTrue(t, myStackOfInts.IsEmpty())
@@ -22,25 +22,13 @@ func TestStack(t *testing.T) {
 		value, _ = myStackOfInts.Pop()
 		AssertEqual(t, value, 123)
 		AssertTrue(t, myStackOfInts.IsEmpty())
-	})
 
-	t.Run("string stack", func(t *testing.T) {
-		myStackOfStrings := new(StackOfStrings)
-
-		// check stack is empty
-		AssertTrue(t, myStackOfStrings.IsEmpty())
-
-		// add a thing, then check it's not empty
-		myStackOfStrings.Push("123")
-		AssertFalse(t, myStackOfStrings.IsEmpty())
-
-		// add another thing, pop it back again
-		myStackOfStrings.Push("456")
-		value, _ := myStackOfStrings.Pop()
-		AssertEqual(t, value, "456")
-		value, _ = myStackOfStrings.Pop()
-		AssertEqual(t, value, "123")
-		AssertTrue(t, myStackOfStrings.IsEmpty())
+		// can get the numbers we put in as numbers, not untyped interface{}
+		myStackOfInts.Push(1)
+		myStackOfInts.Push(2)
+		firstNum, _ := myStackOfInts.Pop()
+		secondNum, _ := myStackOfInts.Pop()
+		AssertEqual(t, firstNum+secondNum, 3)
 	})
 }
 
